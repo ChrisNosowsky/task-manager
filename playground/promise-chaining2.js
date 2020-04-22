112efcd365d459570a52b13adb942ff57bd1b615
Chris <ChrisNosowsky@gmail.com>
2020-04-21 23:59:06 -0400

    challenge

---
+++ playground/promise-chaining2.js
@@ -0,0 +1,12 @@
+require('../src/db/mongoose')
+const Task = require('../src/models/task')
+
+
+Task.findByIdAndRemove('5e968f08a7f19e3648839d41').then((user) => {
+    console.log(user)
+    return Task.countDocuments({completed: false})
+}).then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
