00ab40cbca23731ee0879f889f9c6612cfb6e653
Chris <ChrisNosowsky@gmail.com>
2020-05-22 00:54:39 -0400

    logging out

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-
 const taskSchema = new mongoose.Schema({
     description: {
         type: String,
@@ -12,7 +11,6 @@ const taskSchema = new mongoose.Schema({
         default: false
     }
 })
-
 taskSchema.pre('save', async function (next) {
     const task = this
 
