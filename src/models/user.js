493d92e58ffe0261f3272d41dfea3e3717704d38
Chris <ChrisNosowsky@gmail.com>
2020-05-21 23:53:54 -0400

    c

--- src/models/user.js
+++ src/models/user.js
@@ -59,6 +59,7 @@ userSchema.methods.generateAuthToken = async function() {
     return token
 }
 
+
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 
