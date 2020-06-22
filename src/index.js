3ecfadedeec894c75f63ac72967154cc1260d1b6
Chris <ChrisNosowsky@gmail.com>
2020-06-21 21:59:35 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -31,22 +31,17 @@ app.use(taskRouter)
 
 
 
-
-
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const jwt = require('jsonwebtoken')
 
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days'})
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
+const pet = {
+    name: 'Whiskers'
 }
 
-myFunction()
+pet.toJSON = function() {
+    return {}
+}
+
+console.log(JSON.stringify(pet))
