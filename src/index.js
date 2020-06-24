4e35a128689c86696aac6c74f98f1876884bd856
Chris <ChrisNosowsky@gmail.com>
2020-06-23 22:22:15 -0400

    task user relationship setup between models for figuring out what user created what task.

--- src/index.js
+++ src/index.js
@@ -29,8 +29,20 @@ app.use(taskRouter)
 // without middleware: new request -> run route handler
 // with middleware: new request -> do something -> run route handler
 
-
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+
+const main = async () => {
+    // const task = await Task.findById('5ef2b54d26444f23c8c906da')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5ef2b53826444f23c8c906d6')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+
+}
+
+main()
