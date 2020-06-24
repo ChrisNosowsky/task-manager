5bd67e71b4ca652c8c8509629ae108cfefdb56ee
Chris <ChrisNosowsky@gmail.com>
2020-06-23 22:41:19 -0400

    task endpoints can only be manipulated by logged in user

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,20 +17,21 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
+        const tasks = await Task.find({owner: req.user._id})
         res.send(tasks)
     } catch (e) {
         res.status(500).send(e)
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({_id, owner: req.user._id})
+
         if (!task) {
             return res.status(404).send()
         }
@@ -40,9 +41,9 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({_id: req.params.id, owner: req.user._id})
         
         if(!task) {
             return res.status(404).send()
@@ -54,7 +55,7 @@ router.delete('/tasks/:id', async (req, res) => {
 })
 
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['completed', 'description']
     const _id = req.params.id
@@ -65,15 +66,12 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-
-        const task = await Task.findById(req.params.id)
-
-        updates.forEach((update) => task[update] = req.body[update])
-        await task.save()
-        //const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})
+        const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
         if (!task) {
             return res.status(404).send()
         }
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
         res.send(task)
     } catch(e) {
         res.status(400).send(e)
