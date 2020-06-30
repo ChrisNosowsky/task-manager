29961a8f9fc6358c8980565068377eeca88950ac
Chris <ChrisNosowsky@gmail.com>
2020-06-30 16:57:16 -0400

    initial middleware config for multer file upload setup

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
+const multer = require('multer')
+
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload', upload.single(), (req, res) => {
+    res.send()
+})
 
 // app.use((req, res, next) => {
 //     if (req.method === 'GET') {
