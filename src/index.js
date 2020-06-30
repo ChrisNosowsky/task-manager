6d31635e2040aeefb14ff5fbcd2c204a06fcbca2
Chris <ChrisNosowsky@gmail.com>
2020-06-30 17:47:03 -0400

    remove additional comments

--- src/index.js
+++ src/index.js
@@ -25,19 +25,6 @@ app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
 })
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('Get requests are disabled')
-//     } else {
-//         next()
-//     }
-//     next()
-// })
-
-// app.use((req, res, next) => {
-//     res.send('Site currently under maintenance. Please check back later.').status(503)
-// })
-
 
 app.use(express.json())
 app.use(userRouter)
@@ -50,17 +37,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-
-// const main = async () => {
-//     // const task = await Task.findById('5ef2b54d26444f23c8c906da')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('5ef2b53826444f23c8c906d6')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-
-// }
-
-// main()
