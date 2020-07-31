93bad5284098b55e188e1f70d907c9146eb3ace3
Chris <ChrisNosowsky@gmail.com>
2020-07-30 22:17:07 -0400

    test case working with app.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,10 @@
 const request = require('supertest')
+const app = require('../src/app')
+
+test('should sign up a new user', async () => {
+    await request(app).post('/users').send({
+        name: 'Chris Nosowsky',
+        email: 'nosowsky@gmail.com',
+        password: 'Admin1234!'
+    }).expect(201)
+})
