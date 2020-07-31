72662fd292e4c9606e9772d7c1c025e2a7228fcb
Chris <ChrisNosowsky@gmail.com>
2020-07-30 23:02:27 -0400

    setup and teardown tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    'name': 'Mariah',
+    'email': 'mariah@gmail.com',
+    'password': 'Test123!'
+}
+
+beforeEach(async () => { // Setup
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('should sign up a new user', async () => {
     await request(app).post('/users').send({
@@ -7,4 +19,18 @@ test('should sign up a new user', async () => {
         email: 'nosowsky@gmail.com',
         password: 'Admin1234!'
     }).expect(201)
+})
+
+test('should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('should not login wrong credentials', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: 'wrong'
+    }).expect(400)
 })
