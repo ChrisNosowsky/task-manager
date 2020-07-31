f8d75ec37b6b00050ccc0de5fdf659df6aed38dd
Chris <ChrisNosowsky@gmail.com>
2020-07-30 21:56:24 -0400

    code refactoring

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-const express = require('express')
-require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 // without middleware: new request -> run route handler
 // with middleware: new request -> do something -> run route handler
